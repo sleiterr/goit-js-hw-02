2351b336c18e638ecc96d075f2c2ce43c8d0e917
Oleg Troian <sleite4r@gmail.com>
2024-03-18 10:51:52 +0100

    update

--- task-1.js
+++ task-1.js
@@ -1,22 +1,5 @@
 //* Task 1 Замовлення дроїдів
 
-// 1. оголосити функцію makeTransaction(quantity, pricePerDroid, customerCredits) яка складає та повертає повідомлення про купівлю ремонтних дроїдів.
-
-// 2. Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
-
-// - quantity — кількість замовлених дроїдів
-// - pricePerDroid — ціна одного дроїда
-// - customerCredits — сума коштів на рахунку клієнта
- 
-//* Оголоси змінну для зберігання загальної суми замовлення (загальна вартість усіх замовлених дроїдів) і задай їй вираз розрахунку цієї суми. (const totalPrice = quantity * pricePerDroid;)
-
-//* Додати перевірку, чи зможе клієнт оплатити замовлення: якщо сума до сплати перевищує ">" кількість кредитів на рахунку клієнта "customerCredits",
-
-//*  - функція має повертати рядок "Insufficient funds!" в іншому випадку
-//*  - функція має повертати рядок "You ordered <quantity> droids worth <totalPrice> credits!", де < quantity > це кількість замовлених дроїдів, а < totalPrice > це їх загальна вартість.
-
-
-
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
   if (totalPrice <= customerCredits) {
--- task-3.js
+++ task-3.js
@@ -1,9 +1,5 @@
 //* 3. Перевірка спаму
 
-// створити функцію checkForSpam(message) яка приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
-
-//* Доповни код функції таким чином, що: Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true Якщо в рядку відсутні заборонені слова, функція повертає буль false
-
 function checkForSpam(message) {
   const LowerCaseMessage = message.toLowerCase();
   return LowerCaseMessage.includes("sale") || LowerCaseMessage.includes("spam")
